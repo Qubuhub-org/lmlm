4444191c686dba5ab7e1f9a5d7510edaf7bd4dc4
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-04-01 06:54:42 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -4,3 +4,10 @@ echo "FROM llama3.2" >> Modelfile
 echo "SYSTEM You are a friendly assistant." >> Modelfile
 ollama create -f Modelfile lmlm/Lmkm
 ollama push lmlm/Lmkm
+
+ollama pull llama3.2
+echo "FROM llama3.2" >> Modelfile
+echo "SYSTEM You are a friendly assistant." >> Modelfile
+ollama create -f Modelfile lmlm/Ollama.ai
+ollama push lmlm/Ollama.ai
+ollama run lmlm/Ollama.ai
